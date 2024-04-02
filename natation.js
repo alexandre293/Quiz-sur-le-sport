4e9be3c60760e252b9980e1f41a22d9b86474578
alex <alexlivens09@gmail.com>
2024-04-02 21:09:52 +0200

    modification

--- natation.js
+++ natation.js
@@ -1,20 +1,20 @@
 const correctAnswers = {
     q1: 'a', // Question 1
     q2: 'b', // Question 2
-    q3: 'a', // Question 3
+    q3: 'd', // Question 3
     q4: 'a', // Question 4
-    q5: 'a', // Question 5
-    q6: 'a', // Question 6
-    q7: 'a', // Question 7
+    q5: 'b', // Question 5
+    q6: 'b', // Question 6
+    q7: 'd', // Question 7
     q8: 'a', // Question 8
     q9: 'a', // Question 9
-    q10: 'a', // Question 10
+    q10: 'b', // Question 10
     q11: 'a', // Question 11
     q12: 'b', // Question 12
-    q13: 'a', // Question 13
-    q14: 'a', // Question 14
-    q15: 'a', // Question 15
-    q16: 'a', // Question 16
+    q13: 'b', // Question 13
+    q14: 'c', // Question 14
+    q15: 'b', // Question 15
+    q16: 'b', // Question 16
     q17: 'a', // Question 17
     q18: 'a', // Question 18
     q19: 'a', // Question 19
